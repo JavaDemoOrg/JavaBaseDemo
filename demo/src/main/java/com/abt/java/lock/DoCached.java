6397a3cb65dc79dcc1bfa18218c71a123f4cb3fa
huangweiqi <askviky2010@gmail.com>
2018-05-14 00:17:00 +0800

    新增DoReadWriteLock

---
+++ demo/src/main/java/com/abt/java/lock/DoCached.java
@@ -0,0 +1,56 @@
+package com.abt.java.lock;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Created by huangweiqi on 13/05/2018.
+ */
+public class DoCached {
+
+    public static void main(String[] args) {
+        final Cached ca = new Cached();
+        for (int i = 0; i < 4; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String threadName = Thread.currentThread().getName();
+                    System.out.println(threadName + " " + ca.getS("key"));
+                    System.out.println(threadName + " " + ca.cacheMap.entrySet());
+                }
+            }).start();
+        }
+    }
+
+    /** 多线程实现缓存 */
+    private static class Cached {
+        volatile Map<String, String> cacheMap = new HashMap<String, String>();  // 加volatile关键字保证可见性。
+        ReadWriteLock rwLock = new ReentrantReadWriteLock(); // 这个读写锁要定义在方法外面，使得每一个线程用的是同一个读写锁。
+        public String getS(String key) { //如果定义在方法内部，就是跟方法栈有关的读写锁。这样可能不是同一个锁。
+            rwLock.readLock().lock();
+            String value = null;
+            try {
+                value = cacheMap.get(key);
+                if (cacheMap.get(key) == null) { // 这里要重新获得key对应的value值
+                    rwLock.readLock().unlock();
+                    rwLock.writeLock().lock();
+                    try {
+                        if (cacheMap.get(key) == null) { // 这里也是
+                            value = "sampleValue";
+                            cacheMap.put(key, value);
+                            System.out.println(Thread.currentThread().getName() + " put the value ::: " + value);
+                        }
+                    } finally {
+                        rwLock.readLock().lock();       // 将锁降级，这里跟下一句的顺序不能反。
+                        rwLock.writeLock().unlock();    // 关于这里的顺序问题，下面我会提到。
+                    }
+                }
+            } finally {
+                rwLock.readLock().unlock();
+            }
+            return cacheMap.get(key);
+        }
+    }
+}
--- demo/src/main/java/com/abt/java/lock/DoReadWriteLock.java
+++ demo/src/main/java/com/abt/java/lock/DoReadWriteLock.java
@@ -12,7 +12,7 @@ public class DoReadWriteLock {
     public static void main(String[] args) {
         final DefQueue queue = new DefQueue();
         for (int i = 1; i < 10; i++) {
-            //启动线程进行读操作
+            // 启动线程进行读操作
             new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -22,7 +22,7 @@ public class DoReadWriteLock {
                 }
             }).start();
 
-            //启动线程进行写操作
+            // 启动线程进行写操作
             new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -37,39 +37,31 @@ public class DoReadWriteLock {
     private static class DefQueue {
         private int data;
         ReadWriteLock rwLock = new ReentrantReadWriteLock();
-
         public void get() {
-            rwLock.readLock().lock();//加读锁
+            rwLock.readLock().lock(); // 加读锁
             try {
-                System.out.println(Thread.currentThread().getName() + "be ready to get data");
+                System.out.println(Thread.currentThread().getName() + " be ready to get data");
                 Thread.sleep((long) (Math.random() * 1000));
-
-                System.out.println(Thread.currentThread().getName() + "get the data:    " + data);
-
+                System.out.println(Thread.currentThread().getName() + " get the data:    " + data);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
-                rwLock.readLock().unlock();//释放读锁
+                rwLock.readLock().unlock(); // 释放读锁
             }
         }
 
         public void put(int data) {
-            rwLock.writeLock().lock();//加写锁
-
+            rwLock.writeLock().lock(); // 加写锁
             try {
                 System.out.println(Thread.currentThread().getName() + " be ready to write data");
-
                 Thread.sleep((long) (Math.random() * 1000));
-
                 this.data = data;
-
                 System.out.println(Thread.currentThread().getName() + " has wrote the data:  " + data);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
-                rwLock.writeLock().unlock();//释放写锁
+                rwLock.writeLock().unlock(); // 释放写锁
             }
-
         }
     }
 }
