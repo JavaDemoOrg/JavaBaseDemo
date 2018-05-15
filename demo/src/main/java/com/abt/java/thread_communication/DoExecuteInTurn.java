837bd2914ddbbed0d112f6f96e3f2aca4c1e2b56
hwq <weiqi.huang@pisofttech.com>
2018-05-15 12:26:02 +0800

    添加DoExecuteInTurn

---
+++ demo/src/main/java/com/abt/java/thread_communication/DoExecuteInTurn.java
@@ -0,0 +1,33 @@
+package com.abt.java.thread_communication;
+
+/**
+ * @描述： @
+ * @作者： @黄卫旗
+ * @创建时间： @2018/5/15
+ */
+public class DoExecuteInTurn {
+
+    public static void main(String[] args) {
+        PrintRunnable runnable = new PrintRunnable();
+        Thread threadA = new Thread(runnable, "ThreadA");
+        Thread threadB = new Thread(runnable, "ThreadB");
+        threadA.start();
+        try {
+            threadA.join(); // 关键
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        threadB.start();
+    }
+
+    static class PrintRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i=0; i<3; i++) {
+                String threadName = Thread.currentThread().getName();
+                System.out.println(threadName +" --> print:"+ (i+1));
+            }
+        }
+    }
+}
