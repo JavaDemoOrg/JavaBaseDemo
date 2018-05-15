f9cc1c453dddc4322645d8f3ec0a2d012cbe722e
hwq <weiqi.huang@pisofttech.com>
2018-05-15 16:00:46 +0800

    添加DoRandom

---
+++ demo/src/main/java/com/abt/java/math/DoRandom.java
@@ -0,0 +1,15 @@
+package com.abt.java.math;
+
+/**
+ * @描述： @DoRandom
+ * @作者： @黄卫旗
+ * @创建时间： @2018/5/15
+ */
+public class DoRandom {
+
+    /** Math.random()函数返回的值大于等于0.0小于1.0所以包括0不包括1*/
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("Math.random() = "+Math.random());
+    }
+}
