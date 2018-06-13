1e72eb8f956bce8dc63a98f6685b9aaab2861808
huangweiqi <askviky2010@gmail.com>
2018-06-13 18:13:28 +0800

    添加TestHash

---
+++ demo/src/main/java/com/abt/java/hash/TestHash.java
@@ -0,0 +1,41 @@
+package com.abt.java.hash;
+
+/**
+ * @描述： @TestHash
+ * @作者： @黄卫旗
+ * @创建时间： @13/06/2018
+ */
+public class TestHash {
+
+    public static void main(String[] args) {
+        testHash();
+    }
+
+    private static void testHash() {
+        TestHash test = new TestHash();
+        String str1 = new String("gdejicbeghijk");//gdejicbegh
+        String str2 = new String("hgebcijedgijk");//hgebcijedg
+        System.out.println("hashCode = "+str1.hashCode());//test.hashCode(str1));
+        System.out.println("hashCode = "+str2.hashCode());//test.hashCode(str2));
+
+        System.out.println("str1==str2 : "+(str1==str2));
+        System.out.println("str1.equals(str2) : "+ str1.equals(str2));
+    }
+
+    private int hash = 0;
+    private int hashCode(String str) {
+        System.out.println("hashCode str = "+str);
+        System.out.println("hashCode hash = "+hash);
+        int h = hash;
+        final int len = str.length();
+        if (h == 0 && len > 0) {
+            for (int i = 0; i < len; i++) {
+                h = 31 * h + str.charAt(i);
+                System.out.println("str.charAt("+i+") = "+str.charAt(i)*1);
+            }
+            hash = h;
+        }
+        return h;
+    }
+
+}
---
+++ demo/src/main/java/com/abt/java/hash/hash.md
@@ -0,0 +1,5 @@
+
+
+## hash参考文献   
+1、[关于Java中String类的hashCode方法](https://www.cnblogs.com/micrari/p/5626202.html)    
+2、
