2a3c88ca115d04d16d8d417a143f4af06b27a75d
huangweiqi <askviky2010@gmail.com>
2018-06-19 22:57:22 +0800

    测试sparse-array

---
+++ android/src/main/java/com/abt/android/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.abt.android.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.abt.android.hash_map.TestHashMap;
+
+/**
+ * @描述： @MainActivity
+ * @作者： @黄卫旗
+ * @创建时间： @19/06/2018
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TestSparseArray.testSparseArray();
+        //TestSparseArray.testMemoryTime();
+        TestHashMap.testHashMap();
+    }
+}
