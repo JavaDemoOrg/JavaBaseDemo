6ebb5d96b340a11ede9032884fdc99d2d16ecc87
huangweiqi <askviky2010@gmail.com>
2018-06-19 22:57:02 +0800

    完善sparse-array

--- android/src/main/java/com/abt/android/sparse_array/TestSparseArray.java
+++ android/src/main/java/com/abt/android/sparse_array/TestSparseArray.java
@@ -2,28 +2,52 @@ package com.abt.android.sparse_array;
 
 import android.util.SparseArray;
 
+import com.abt.android.util.TimeUtil;
+
 /**
  * @描述： @TestSparseArray
  * @作者： @黄卫旗
  * @创建时间： @16/06/2018
  */
 public class TestSparseArray {
-
-    private static int MAX = 100000;
+    //06-19 22:32:41.983 20935-20935/com.abt.android I/System.out: 2018-06-19 22:32:28:500
+    //06-19 22:32:41.983 20935-20935/com.abt.android I/System.out: 2018-06-19 22:32:41:989
+    //06-19 22:32:41.993 20935-20935/com.abt.android I/System.out: GetTime: 146667
+    private static int MAX = 1000000;
 
     public static void main(String[] args) {
-        SparseArray sparse = new SparseArray(MAX);
-        //SparseIntArray sparse = new SparseIntArray(MAX);
-        long start_sparse = System.currentTimeMillis();
-        for(int i=0;i<MAX;i++){
-            //sparse.put(i, String.valueOf(i));
-            sparse.put(i, i);
-        }
-        long sparse_memory = Runtime.getRuntime().totalMemory();
-        long end_sparse = System.currentTimeMillis()-start_sparse;
-        System.out.println("<---Sparse的插入时间--->"+end_sparse+
-                "<---Sparse占用的内存--->"+sparse_memory);
+        //testSparseArray();
+        testMemoryTime();
     }
 
+    public static final void testSparseArray() {
+        SparseArray sparseArray = new SparseArray();
+        String startStr = TimeUtil.getFormatStr(System.currentTimeMillis());
+        for (int i=0; i<MAX; i++) {
+            sparseArray.put(i, "value-"+i);
+            //System.out.println("key-"+i+" value-"+i);
+        }
+        String endStr = TimeUtil.getFormatStr(System.currentTimeMillis());
+        System.out.println(startStr);
+        System.out.println(endStr);
+
+        long start = System.nanoTime();
+        String res = (String) sparseArray.get(999995);
+        long end = System.nanoTime();
+        System.out.println("GetTime: " + (end - start));
+        System.out.println("res: "+res);
+    }
+
+    public static final void testMemoryTime() {
+        SparseArray sparse = new SparseArray(MAX);
+        long startSparse = System.currentTimeMillis();
+        for(int i=0;i<MAX;i++){
+            sparse.put(i, i);
+        }
+        long sparseMemory = Runtime.getRuntime().totalMemory();
+        long endSparse = System.currentTimeMillis()-startSparse;
+        System.out.println("<---Sparse的插入时间--->"+endSparse+
+                "<---Sparse占用的内存--->"+sparseMemory);
+    }
 
 }
